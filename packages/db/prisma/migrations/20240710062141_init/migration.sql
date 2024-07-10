3827d11997da007f541c53ea0fc76554bdd3bda7
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-07-10 11:55:21 +0530

    feat: prsima

--- packages/db/prisma/migrations/20240710062141_init/migration.sql
+++ packages/db/prisma/migrations/20240710062141_init/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
